c1dde417111a63c3e470710453c64eef06d4e7d9
Espada23 <akbarma2002@gmail.com>
2020-06-27 23:41:27 +0700

    Script Baru

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var stat = confirm("Kamu mau jadi pacar aku gaa?");
 if (stat == true) {
-    console.log("Yeayy, hari ini kita jadian yaa!");
+   alert("Yeayy, hari ini kita jadian yaa!");
+   break;
 }
 else if (stat == false) {
-    console.log("Anjing kamu, aku benci kamu!");
-}
-else {
-    console.log("Jadi?? Jawabannyaa apaa??");
+    alert("Anjing kamu, aku benci kamu!");
+    break;  
 }
