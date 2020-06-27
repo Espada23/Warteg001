655d124308fe8e468819de3c53565340f91bf1b1
Espada23 <akbarma2002@gmail.com>
2020-06-27 23:16:47 +0700

    4KU Cayankk Kmwuu

---
+++ script.js
@@ -0,0 +1,10 @@
+var stat = confirm("Kamu mau jadi pacar aku gaa?");
+if (stat == true) {
+    console.log("Yeayy, hari ini kita jadian yaa!");
+}
+else if (stat == false) {
+    console.log("Anjing kamu, aku benci kamu!");
+}
+else {
+    console.log("Jadi?? Jawabannyaa apaa??");
+}
