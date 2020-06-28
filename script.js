6ce7a6d0ef7054f263ab79fc4ba8915f38fe3c9c
Akbar Maulana <57890645+Espada23@users.noreply.github.com>
2020-06-28 16:37:23 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var stat = confirm("Kamu mau jadi pacar aku gaa?");
-if (stat == true) {
-   alert("Yeayy, hari ini kita jadian yaa!");
-   break;
-}
-else if (stat == false) {
-    alert("Anjing kamu, aku benci kamu!");
-    break;  
-}
