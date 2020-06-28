57aa01601a2730138f0aa9f6f7a3b4fc43397e7d
etrnal70 <etrnal70@gmail.com>
2020-06-28 20:56:26 +0700

    Fix start button logic

--- timer.js
+++ timer.js
@@ -4,6 +4,7 @@ var hour = 0;
 var minute = 0
 function reset() {
     document.getElementById("timer").innerHTML = 0 + ":" + 0 + ":" + 0;
+    document.getElementById('start').value = "START";
     status = 0;
     second = 0;
     hour = 0;
@@ -15,12 +16,12 @@ function start() {
     if(status == 0){
         status = 1;
         inc();
-        document.getElementById('start').value = "Pause";
+        document.getElementById('start').value = "PAUSE";
         document.getElementById('start').style.backgroundColor = "red";
         document.getElementById('start').style.borderColor = "black";
     }else{
         status = 0;
-        document.getElementById('start').value = "Resume";
+        document.getElementById('start').value = "RESUME";
         document.getElementById('start').style.backgroundColor = "green";
         document.getElementById('start').style.borderColor = "black";
     }
