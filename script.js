ccc1a691e1529ee288f2104efba105cedda214fb
etrnal70 <etrnal70@gmail.com>
2020-06-28 17:10:36 +0700

    Revert 6ce7a6d0ef7054f263ab79fc4ba8915f38fe3c9c

---
+++ script.js
@@ -0,0 +1,9 @@
+var stat = confirm("Kamu mau jadi pacar aku gaa?");
+if (stat == true) {
+   alert("Yeayy, hari ini kita jadian yaa!");
+   break;
+}
+else if (stat == false) {
+    alert("Anjing kamu, aku benci kamu!");
+    break;  
+}
